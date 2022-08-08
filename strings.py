593a657415ed2b33ec8a3a85d12314c6959de17a
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-08-08 18:32:08 +0300

    string practice

--- strings.py
+++ strings.py
@@ -4,3 +4,41 @@ print(string)
 x=10
 y=7
 print(x-y)
+print('Hello World!')
+string_0 ='Hello World!'
+print(string_0)
+string_1 ='Good'
+string_2 =' Morning'
+print(string_1, string_2)
+print('Hello QAForEveryone!')
+string_3 = 'Hello QAForEveryone!'
+print(string_3)
+
+a='------------'
+b='|   *  *   |'
+c='|          |'
+d='|   ____   |'
+e='------------'
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print('Python is cool')
+str ='Python is cool'
+print(str)
+
+print(' '*6 +'*')
+print(' '*4 +'*'*4)
+print(' '*2 +'*'*8)
+print(' ' +'*'*10)
+print(' '*5 +'*')
+
+print('_'*10)
+print('|',' '*6,'|')
+print('|',' '*6,'|')
+print('|',' '*6,'|')
+print('-'*10)
+
+print('_'*10+'\n'+'|',' '*6,'|'+'\n'+'|',' '*6,'|'+'\n'+'|',' '*6,'|'+'\n'+'-'*10)
